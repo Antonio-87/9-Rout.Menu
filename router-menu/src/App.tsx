64b0d0ec98a7f75b8fdf4de077d9ccdde61703c1
Antonio-87 <davydenko.87@list.ru>
2024-01-14 14:40:21 +0300

    invironment

---
+++ router-menu/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
