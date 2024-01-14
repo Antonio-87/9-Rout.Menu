3b59d1060ad11c1ae5605c8e09e0ac0e49e0b4b8
Antonio-87 <davydenko.87@list.ru>
2024-01-14 15:18:47 +0300

    HomePage_DriftPage_TimeAttackPage_ForzaPage

--- router-menu/src/App.tsx
+++ router-menu/src/App.tsx
@@ -1,19 +1,33 @@
 import "./App.css";
+import DriftPage from "./components/DriftPage";
+import ForzaPage from "./components/ForzaPage";
+import HomePage from "./components/HomePage";
+import Menu from "./components/Menu";
+import TimeAttackPage from "./components/TimeAttackPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <div>
+      <Menu />
+      <div className="page">
+        <Route path="/" element={<HomePage />} />
+        <Route path="/drift" element={<DriftPage />} />
+        <Route path="/timeattack" element={<TimeAttackPage />} />
+        <Route path="/forza" element={<ForzaPage />} />
+      </div>
+    </div>
+  )
+);
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Menu />
-        <div className="page">
-          <Route path="/" exact component={HomePage} />
-          <Route path="/drift" component={DriftPage} />
-          <Route path="/timeattack" component={TimeAttackPage} />
-          <Route path="/forza" component={ForzaPage} />
-        </div>
-      </div>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
